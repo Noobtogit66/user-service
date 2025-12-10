d59af1fd60f5690d25690858b6df5a81ef567483
Noobtogit66 <janasagnik66@gmail.com>
2025-12-10 20:42:39 +0530

    JPA dependency added

--- src/main/java/com/sagnik/democlient/repository/UserRepository.java
+++ src/main/java/com/sagnik/democlient/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.sagnik.democlient.repository;
 
 import com.sagnik.democlient.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
