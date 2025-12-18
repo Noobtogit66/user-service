5eaeae4a80204d85c270ee1fa8cfb39297411153
Noobtogit66 <janasagnik66@gmail.com>
2025-12-18 09:03:04 +0530

    priliminary framework

--- src/main/java/com/sagnik/democlient/controller/UserController.java
+++ src/main/java/com/sagnik/democlient/controller/UserController.java
@@ -1,22 +1,43 @@
 package com.sagnik.democlient.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-
+import com.sagnik.democlient.model.User;
+import com.sagnik.democlient.service.UserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+@Slf4j
 @RestController("/user-service")
 public class UserController {
+@Autowired
+private UserService userService;
 
+@GetMapping("/getUser/{userId}")
+    public ResponseEntity getUser(@PathVariable String userId)
+{
+    return ResponseEntity.status(HttpStatus.ACCEPTED).body(userService.getUserById(userId));
+}
 
-    //get User by id
+@GetMapping("/getUserList")
+    public ResponseEntity getUserList()
+{
+    return ResponseEntity.status(HttpStatus.ACCEPTED).body(userService.getAllUser());
+}
 
+@PostMapping("/addUser")
+    public ResponseEntity addUser(@RequestBody User user)
+{
+    log.info(user.toString());
+    return ResponseEntity.status(HttpStatus.OK).body(userService.addUser(user));
+}
+@DeleteMapping("/deleteUser/{userId}")
+public ResponseEntity deleteUser(@PathVariable String userId)
+{
+    userService.deleteUser(userId);
+    return ResponseEntity.status(HttpStatus.OK).body(null);
+}
 
-
-//    Get All user
-
-//    Add user
-
-//    Delete user
-
-
-//    Edit user
 }
 
--- src/main/java/com/sagnik/democlient/service/UserService.java
+++ src/main/java/com/sagnik/democlient/service/UserService.java
@@ -31,4 +31,6 @@ public class UserService {
         userRepository.deleteById(userId);
     }
 
+
+
 }
